918986ede480d8dbdb0dc940440f71e46abba823
aria <122402951+usernamearia@users.noreply.github.com>
2024-08-06 21:27:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('contact-form');
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        alert('Form submitted! Thank you for reaching out.');
-        form.reset();
-    });
-});
+
