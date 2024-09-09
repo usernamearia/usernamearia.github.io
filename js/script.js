aba5a03a2500350c1f1f6ffffadd219ddd432e18
Aria <ariasafaie@gmail.com>
2024-09-09 12:02:48 -0400

    Added links to the project section and actually got the js file linked/working properly

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Personal Website Project
+  const personalWebsiteProject = document.getElementById('personal-website-project');
+  personalWebsiteProject.addEventListener('click', function () {
+      alert("You're already here :)");
+  });
+});
