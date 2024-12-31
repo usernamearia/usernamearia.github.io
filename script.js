c2b6d1f2c42b113856da9057cbb8d4bf7dc0631f
aria <ariasafaie@gmail.com>
2024-12-30 20:15:29 -0500

    mega fix
    everything will be fine

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const nameElement = document.getElementById('mode-toggle');
-const body = document.body;
-
-nameElement.addEventListener('click', () => {
-    const isDarkMode = body.classList.toggle('dark-mode');
-    body.classList.toggle('light-mode', !isDarkMode);
-});
-
 function copyEmail() {
     navigator.clipboard.writeText('ariasafaie@gmail.com')
         .then(() => alert('Email copied to clipboard!'))
